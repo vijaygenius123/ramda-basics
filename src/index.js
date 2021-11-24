0e0455be6e12ec3fac5dec3913d1bd4b93480cf5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-24 12:28:08 +0000

    Final

--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
 import fs from "fs";
-import R from "ramda";
+import {
+    converge,
+    concat,
+    replace,
+    pipe,
+    split,
+    dropLast,
+    map,
+    take,
+    __,
+    view,
+    lensIndex,
+    objOf,
+    head,
+    tail,
+    transpose, mergeAll
+} from "ramda";
 
 const readFile = path => fs.readFileSync(path, 'utf-8')
 
+const readData = converge(concat,
+            [
+                pipe(
+                    replace(/.*\//, ''),
+                    replace(/\..*/, ''),
+                    concat('date\t'),
+                    concat(__, '\n'),
+                ), readFile
+            ])
 
-R.pipe(
-    R.map(
-    R.pipe(
-    readFile,
-    R.split('\n'),
-    R.dropLast(1),
-    R.map(R.split('\t')),
-    R.map(R.take(2)),
+pipe(
+    map(
+        pipe(
+            readData,
+            split('\n'),
+            dropLast(1),
+            map(split('\t')),
+            map(take(2)),
+            map(view(lensIndex(1))),
+            converge(map, [
+                pipe(
+                    head,
+                    objOf
+                ), tail
+            ]),
+        )
+    ),
+    transpose,
+    map(mergeAll),
     console.log
-    )
-    )
 )
 (['./input/btc.csv', './input/eth.csv'])
