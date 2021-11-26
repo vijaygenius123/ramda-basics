f9a24e35067c9490c955b5f16715fd1f0d32b028
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-26 21:44:12 +0000

    Cities - Convert Temp

--- src/cities.js
+++ src/cities.js
@@ -1,14 +1,21 @@
 import fs from "fs";
-import {pipe, filter, propEq} from "ramda";
+import {pipe, map, prop} from "ramda";
 
 
 const readFile = path => fs.readFileSync(path, {encoding: 'utf-8'})
 
 const parse = data => JSON.parse(data)
+const convertKToC = temp => temp - 273.15
+const convertCToF = temp =>  (convertKToC(temp) * 9/5 ) + 32
+
 
 pipe(
     readFile,
     parse,
-    filter(propEq('country', 'UK')),
+    map(
+        prop('temp'),
+        convertCToF
+        ),
+   // filter(propEq('country', 'UK')),
     console.log
 )('./input/cities.json')
