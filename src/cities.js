f1e313c481b1e39c4d543b2f842a22d32343292d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-29 09:31:57 +0000

    Group internetSpeed & cost props

--- src/cities.js
+++ src/cities.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import {pipe, map, prop} from "ramda";
+import {pipe, map, prop, curry, append, mergeRight, reduce} from "ramda";
 
 
 const readFile = path => fs.readFileSync(path, {encoding: 'utf-8'})
@@ -7,15 +7,29 @@ const readFile = path => fs.readFileSync(path, {encoding: 'utf-8'})
 const parse = data => JSON.parse(data)
 const convertKToC = temp => temp - 273.15
 const convertCToF = temp =>  (convertKToC(temp) * 9/5 ) + 32
+const updateTemperature = curry((conversionFunc ,city) => {
+        const temp = conversionFunc(prop('temp', city))
+        return mergeRight(city, {temp})
+})
 
-
-pipe(
+const citiesData = pipe(
     readFile,
     parse,
-    map(
-        prop('temp'),
-        convertCToF
-        ),
-   // filter(propEq('country', 'UK')),
-    console.log
 )('./input/cities.json')
+const updatedCities = map(
+        updateTemperature(convertKToC)
+    )(citiesData)
+
+//console.log(updatedCities)
+
+const groupByReducer = (acc, city) => {
+        const {cost = [], internetSpeed = []} = acc;
+        return mergeRight(acc, {
+                cost: append(prop('cost', city), cost),
+                internetSpeed: append(prop('internetSpeed', city), internetSpeed)
+        })
+}
+
+const groupedByProp = reduce(groupByReducer, {}, updatedCities)
+
+console.log(groupedByProp)
